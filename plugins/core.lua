61bfac8b1b990c41ce2453284f4eb27640b0422c
Mr.X <79870712+n30mrx@users.noreply.github.com>
2023-09-24 01:35:56 +0300

    yeah just the normal "hotfix"

--- plugins/core.lua
+++ plugins/core.lua
@@ -77,5 +77,26 @@ return {
   {
     'Mofiqul/dracula.nvim',
     lazy=false,
-  }
+  },
+  {
+    "nvim-neorg/neorg",
+    lazy=false,
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
 }
